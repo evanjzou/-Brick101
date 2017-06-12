6c1633fd7a1d0f65dfec59fa72ac3f6b4df9bd13
Evan Zou <Evan.Zou@neulion.local>
2017-06-12 09:56:21 -0400

    Creating basic framework

--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@
 	Tem
 */
 
-//Testing merging code with Evan
+/*/Testing merging code with Evan
 
 
 console.log("Hello world");
 
 console.log('learning js');
 
-alert("Tem has to remember his password");
+alert("Tem has to remember his password"); //*/
+
+/*Main script for the game. Game instance is instantiated here */
+
+//Initializes the game
+function init() {
+	//TODO
+}
